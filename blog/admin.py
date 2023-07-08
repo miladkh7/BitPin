cefea67f89a6cad0ddb39ee0787808256e9097f9
milad <milad_khaleghi@live.com>
2023-07-08 11:02:38 +0330

    IMP(blog): add rate model admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Rate
 
 admin.site.register(Article)
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ["article", "rate", "user"]
