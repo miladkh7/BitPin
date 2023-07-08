d0efb9784a398ec1b52ae19b37ef1f7453fa63bd
milad <milad_khaleghi@live.com>
2023-07-08 09:48:09 +0330

    CHORE(setting): config develop tools

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+LOCAL_APPS = []
+THIRD_PARTY_APPS = []
+DEV_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + THIRD_PARTY_APPS + DEV_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
