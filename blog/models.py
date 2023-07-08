8898789a4e29e4a2e9d363fdb984f979c141c016
milad <milad_khaleghi@live.com>
2023-07-08 10:56:44 +0330

    FEAT(blog): add article model and its migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+#Django
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# from 
+
+class Article(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, default='Title')
+    content = models.TextField(null=True)
+    
+    def __str__(self) -> str:
+        return f"{self.author}-{self.title}"
