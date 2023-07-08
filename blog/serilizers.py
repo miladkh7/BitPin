076aca0aa0a51c505f8905d2772c0c8eb59796f3
milad <milad_khaleghi@live.com>
2023-07-08 12:10:36 +0330

    FEAT(blog): rate api

--- blog/serilizers.py
+++ blog/serilizers.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 #local
-from .models import Article
+from .models import Article, Rate
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +21,14 @@ class ArticleSerializer(serializers.ModelSerializer):
         user_id = self.context['user_id']
         user = get_object_or_404(get_user_model(), pk=user_id)
         return Article.objects.create(author=user, **validated_data)
+    
+
+class RateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rate
+        field = ['article', 'rate']
+
+    def create(self, validated_data):
+        user_id = self.context['user_id']
+        user = get_object_or_404(get_user_model(), pk=user_id)
+        return Rate.objects.create(author=user, **validated_data)
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@ from django.db.models.aggregates import Avg, Count
 
 #Third party
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly,IsAuthenticated
-
+from rest_framework import status
+from rest_framework.response import Response
 #Local
 from .models import Article
-from .serilizers import ArticleSerializer
+from .serilizers import ArticleSerializer, RateSerializer
 
 
 
@@ -29,3 +31,11 @@ class ArticleViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'user_id':self.request.user.pk}
         # return {'user_id':self.request.user.id}
+
+    @action(methods=['POST'], detail=True)
+    def submit_rate(self,request , pk):
+        article = self.get_object()
+        serializer = RateSerializer(article,data=request.data )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
