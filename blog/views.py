b0ee74559ed017cc4551f0a75d160c6f6090046b
milad <milad_khaleghi@live.com>
2023-07-08 11:35:50 +0330

    IMP(blog): add permission class

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
-from rest_framework.viewsets import ModelViewSet
+
+#Django
 from django.db.models.aggregates import Avg, Count
-from .serilizers import ArticleSerializer
+
+#Third party
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+
+#Local
 from .models import Article
-# Create your views here.
+from .serilizers import ArticleSerializer
+
 
 
 class ArticleViewSet(ModelViewSet):
-    
+
     queryset = Article.objects.annotate(
     vote_number=Count('rates'),
     average=Avg('rates__rate')
     ).all()
     serializer_class = ArticleSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        else:
+            return [IsAuthenticatedOrReadOnly()]
