9d0ad16708cbbe3bca712d14d9d8ae38d5f9aebc
milad <milad_khaleghi@live.com>
2023-07-08 09:58:07 +0330

    FEAT(user): add customer user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+# Standard
+import uuid
 
-# Create your models here.
+#Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    """
+    Default custom user model for BitPin.
+    """
+    #: for security reason use uuid4
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    #: First and last name do not cover name patterns around the globe
+    name = CharField(_("Name of User"), blank=True, max_length=255)
+    first_name = None  # type: ignore
+    last_name = None  # type: ignore
+
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("users:detail", kwargs={"username": self.username})
