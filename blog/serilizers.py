7da7565bd16184c181739ad8d0523e877931cebe
milad <milad_khaleghi@live.com>
2023-07-08 11:12:37 +0330

    FEAT(view): add article view set

--- blog/serilizers.py
+++ blog/serilizers.py
@@ -4,7 +4,7 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title','average','vote_number','content']
+        fields = ['id','author', 'title','average','vote_number','content']
 
     average = serializers.FloatField(read_only=True)
     vote_number = serializers.IntegerField(read_only=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from django.db.models.aggregates import Avg, Count
+from .serilizers import ArticleSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from .models import Article
 # Create your views here.
+
+
+class ArticleViewSet(ModelViewSet):
+    
+    queryset = Article.objects.annotate(
+    vote_number=Count('rates'),
+    average=Avg('rates__rate')
+    ).all()
+    serializer_class = ArticleSerializer
