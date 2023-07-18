71d72ce658e6d2be2419daebf8f984da68b742bf
milad <milad_khaleghi@live.com>
2023-07-18 18:15:15 +0330

    IMP(routing): add  drf-spectacular routes

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('auth/', include('djoser.urls')),
@@ -27,5 +30,10 @@ if settings.DEBUG:
     
     urlpatterns_develop = [
         path("__debug__/", include("debug_toolbar.urls")),
+
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        # Optional UI:
+        path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     ]
     urlpatterns += urlpatterns_develop
