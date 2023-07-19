42e0d58f54689e0575b4c0f8a39d4394edea1c9f
milad <milad_khaleghi@live.com>
2023-07-19 12:23:51 +0330

    TEST(blog): change token base authentication for tests

--- blog/tests/test_blog.py
+++ blog/tests/test_blog.py
@@ -1,3 +1,7 @@
+import json
+import random
+import string
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.reverse import reverse
@@ -8,11 +12,31 @@ from ..models import Article
 class BlogTestCase(TestCase):
 
     @classmethod
+    def get_token_from_login(cls,username,password):
+        data = {
+        "password": password,
+        "username": username,
+        }
+
+        response = cls.client.post(
+            path=reverse('login'),
+            data=json.dumps(data),
+            content_type='application/json',
+            follow=True
+
+        )
+        if response.status_code == status.HTTP_200_OK:
+            token= response.data['auth_token']
+            return token
+        return False
+
+    @classmethod
     def setUpTestData(cls) -> None:
         super().setUpTestData()
         cls.client = APIClient()
+        user_authenticated_password= "testp"
         user_authenticated =UserFactory()
-        user_authenticated.set_password('testp')
+        user_authenticated.set_password(user_authenticated_password)
         user_authenticated.save()
         cls.authenticated_user= user_authenticated
 
@@ -20,13 +44,13 @@ class BlogTestCase(TestCase):
         sample_article.author = cls.authenticated_user
         sample_article.save()
         cls.sample_article = sample_article
+        user_authenticated_token_value = cls.get_token_from_login(user_authenticated.username,user_authenticated_password)
+        cls.user_authenticated_token = user_authenticated_token_value
 
 
-    def setUp(self) -> None:
-        
-        return super().setUp()
 
     def test_get_blog_posts(self):
+
             response = self.client.get(
                 path=reverse('article-list'),
                 content_type='application/json',
@@ -35,10 +59,6 @@ class BlogTestCase(TestCase):
             self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_book_with_authenticated_user(self):
-        self.client.login(
-            username=self.authenticated_user.username,
-            password='testp'
-        )
         data = {
             "title": "test_title",
             "content": "test_content",
@@ -47,7 +67,8 @@ class BlogTestCase(TestCase):
             path=reverse('article-list'),
             data=data,
             content_type='application/json',
-            follow=True
+            HTTP_AUTHORIZATION= f'Token {self.user_authenticated_token}',
+            follow=True,
         )
 
         # check response code
@@ -64,15 +85,18 @@ class BlogTestCase(TestCase):
             "title": "test_title",
             "content": "test_content",
         }
+        random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=40))
         response = self.client.post(
             path=reverse('article-list'),
             data=data,
             content_type='application/json',
-            follow=True
+            follow=True,
+            HTTP_AUTHORIZATION= f'Token {random_string}',
+
         )
 
         # check response code
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def get_article(self):
         response = self.client.get(
@@ -86,10 +110,6 @@ class BlogTestCase(TestCase):
         self.assertEqual(response.data['author'], self.sample_article.author.username)
 
     def test_update_article_by_owner(self):
-        self.client.login(
-            username=self.authenticated_user.username,
-            password='testp'
-        )
         data = {
             "title": "test_title2",
             "content": "test_content2",
@@ -98,7 +118,9 @@ class BlogTestCase(TestCase):
             path=reverse('article-detail', kwargs={'pk': self.sample_article.pk}),
             data=data,
             content_type='application/json',
-            follow=True
+            follow=True,
+            HTTP_AUTHORIZATION= f'Token {self.user_authenticated_token}',
+
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], data['title'])
@@ -108,50 +130,46 @@ class BlogTestCase(TestCase):
         user = UserFactory()
         user.set_password('testp')
         user.save()
-        self.client.login(
-            username=user.username,
-            password='testp'
-        )
         data = {
             "title": "test_title2",
             "content": "test_content2",
         }
+        current_user_token= self.get_token_from_login(user.username,'testp')
         response = self.client.put(
             path=reverse('article-detail', kwargs={'pk': self.sample_article.pk}),
             data=data,
             content_type='application/json',
-            follow=True
+            follow=True,
+            HTTP_AUTHORIZATION= f'Token {current_user_token}',
+
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_article_by_owner(self):
-        self.client.login(
-            username=self.authenticated_user.username,
-            password='testp'
-        )
+
         response = self.client.delete(
             path=reverse('article-detail', kwargs={'pk': self.sample_article.pk}),
             content_type='application/json',
-            follow=True
+            follow=True,
+            HTTP_AUTHORIZATION= f'Token {self.user_authenticated_token}',
+
+
+            
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     
     def test_submit_article_rate(self):
-        self.client.login(
-            username=self.authenticated_user.username,
-            password='testp'
-        )
         data = {
             "rate": 5,
         }
+
         response = self.client.post(
-            path=reverse('rate', kwargs={'pk': self.sample_article.pk}),
+            path=reverse('article-submit-rate', kwargs={'pk': self.sample_article.pk}),
             data=data,
             content_type='application/json',
-            follow=True
+            follow=True,
+            HTTP_AUTHORIZATION= f'Token {self.user_authenticated_token}',
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['rate'], data['rate'])
-        self.assertEqual(response.data['article'], self.sample_article.pk)
-        self.assertEqual(response.data['user'], self.authenticated_user.pk)
